3740ef889d1364842451fa10ca0f1b42b72ee920
alejopua <alejopua@gmail.com>
2024-04-17 15:42:16 -0500

    Refactor TasksService to handle localStorage availability

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -9,18 +9,33 @@ export class TasksService {
   constructor() {}
 
   getTasks() {
-    return JSON.parse(localStorage.getItem(this.lStorageKey) as string) || [];
+    if (typeof localStorage !== 'undefined') {
+      // Código que usa localStorage
+      return JSON.parse(localStorage.getItem(this.lStorageKey) as string) || [];
+    } else {
+      console.error('localStorage is not available.');
+    }
   }
 
   addTask(task: string) {
     const tasks = this.getTasks();
     tasks.push(task);
-    localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+    if (typeof localStorage !== 'undefined') {
+      // Código que usa localStorage
+      localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+    } else {
+      console.error('localStorage is not available.');
+    }
   }
 
   deleteTask(taskIndex: number) {
-    const tasks = this.getTasks();
-    tasks.splice(taskIndex, 1);
-    localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+    if (typeof localStorage !== 'undefined') {
+      // Código que usa localStorage
+      const tasks = this.getTasks();
+      tasks.splice(taskIndex, 1);
+      localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+    } else {
+      console.error('localStorage is not available.');
+    }
   }
 }
