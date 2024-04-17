bd11174d15ac59ea86319f5e904b96b1c89d2986
alejopua <alejopua@gmail.com>
2024-04-17 14:48:57 -0500

    Update imports and add TasksService in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from './services/tasks.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taskApp';
+  tasksList: string[] = [];
+  newTask: string = '';
+
+  private _taskService = inject(TasksService);
+
+  ngOnInit(): void {
+    this.tasksList = this._taskService.getTasks();
+  }
 }
