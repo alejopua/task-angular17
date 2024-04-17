88b177795bad572a3bb25f391bc5741dd9eb71b3
alejopua <alejopua@gmail.com>
2024-04-17 16:54:18 -0500

    Update Tailwind CSS configuration to add new color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        "background-100": "#1a1a1a",
+        "background-200": "#2a2a2a",
+        "background-300": "#3a3a3a",
+        "background-400": "#4a4a4a",
+      },
+    },
   },
   plugins: [],
 };
