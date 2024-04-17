df9271630a4820234f5f5c89a89823dbcea1912e
alejopua <alejopua@gmail.com>
2024-04-17 14:24:27 -0500

    Add methods for Logic App: getTasks, addTask, deleteTask

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
+  private lStorageKey = 'tasksList';
 
-  constructor() { }
+  constructor() {}
+
+  getTasks() {
+    return JSON.parse(localStorage.getItem(this.lStorageKey) as string) || [];
+  }
+
+  addTask(task: string) {
+    const tasks = this.getTasks();
+    tasks.push(task);
+    localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+  }
+
+  deleteTask(task: string) {
+    const tasks = this.getTasks();
+    tasks.splice(task, 1);
+    localStorage.setItem(this.lStorageKey, JSON.stringify(tasks));
+  }
 }
