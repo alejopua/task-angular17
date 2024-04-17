53a5bc944f6af134ad4339672be4f30e5b972c75
alejopua <alejopua@gmail.com>
2024-04-17 16:54:52 -0500

    Refactor app.component.ts to import RouterOutlet for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from './services/tasks.service';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  title = 'taskApp';
-  tasksList: string[] = [];
-  newTask: string = '';
-
-  private _taskService = inject(TasksService);
-
-  ngOnInit(): void {
-    this.tasksList = this._taskService.getTasks();
-  }
-
-  deleteTask(taskIndex: number): void {
-    console.log('deleteTask', taskIndex);
-    this._taskService.deleteTask(taskIndex);
-    this.tasksList = this._taskService.getTasks();
-  }
-
-  addTask(newTask: string): void {
-    this._taskService.addTask(newTask);
-    this.newTask = '';
-    this.tasksList = this._taskService.getTasks();
-  }
-}
+export class AppComponent {}
