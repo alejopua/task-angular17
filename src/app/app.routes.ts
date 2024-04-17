14d46479a8411e68dc0ba2c1acdd99f6eda5ca00
alejopua <alejopua@gmail.com>
2024-04-17 16:54:30 -0500

    Add TaskComponent to handle task management in the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from './components/task/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'task', component: TaskComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'task' },
+];
